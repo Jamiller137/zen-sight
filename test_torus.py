ff246d05a694f8929122b605b0c1eaaa13d7c112
Jacob Miller <Jamiller219@gmail.com>
2025-04-06 23:43:49 -0500

    updated test_torus

--- test_torus.py
+++ test_torus.py
@@ -1,3 +1,4 @@
+from networkx import edges
 from zen_mapper import mapper
 from zen_mapper.cover import Width_Balanced_Cover
 from src.zen_sight import SimplexTreeVisualizer
@@ -6,7 +7,7 @@ from zen_mapper.cluster import sk_learn
 import numpy as np
 from matplotlib.pyplot import get_cmap
 
-def create_torus(n_points=5000, R=3.0, r=1.0, noise_level=0.05):
+def create_torus(n_points=20000, R=3.0, r=1.0, noise_level=0.05):
 
     theta = 2 * np.pi * np.random.rand(n_points)
     phi = 2 * np.pi * np.random.rand(n_points)
@@ -47,8 +48,10 @@ def value_to_hex_color(value:float, min_val=-1.0, max_val=1.0, colormap='viridis
 if __name__ == '__main__':
 
     dataset = create_torus()
-    projection = np.column_stack((dataset[:,0], dataset[:,2]))
-    coverer = Width_Balanced_Cover(n_elements=5, percent_overlap=0.3)
+    projection = dataset 
+    # no projection since picture looks nicer!
+    # if dataset[:, :2] then things are squished but equivalent
+    coverer = Width_Balanced_Cover(n_elements=5, percent_overlap=0.25)
 
     result = mapper(
         data=dataset,
@@ -60,13 +63,13 @@ if __name__ == '__main__':
     )
     nodes = result.nodes
     simplex_tree = result.nerve
-    viz = SimplexTreeVisualizer(simplex_tree, max_dim=3)
+    viz = SimplexTreeVisualizer(simplex_tree, max_dim=2)
     print(dataset[0])
     viz.set_default_materials(
         material_defaults = {
             "vertices": {
                 "color": 0x005ebb, #blue
-                "size": 15,
+                "size": 2,
                 "opacity": 1,
             },
             "edges": {
@@ -76,32 +79,62 @@ if __name__ == '__main__':
                 "dashed": False,
             },
             "faces": {
-                "color": 0x3366CC, #red
-                "opacity": 0.4,
+                "color": 0x3366CC, #blue
+                "opacity": .8,
             },
             "tetrahedra": {
                 "color": 0x000001,
                 "opacity": 0.8,
             }
         })
-    
+    deleted_vertex_set = set()
     for i in range(len(nodes)):
         data_indices = nodes[i]
         cluster_data = dataset[data_indices]
         avg_x_value = np.average(cluster_data[:,0])
         avg_y_value = np.average(cluster_data[:,1])
         avg_z_value = np.average(cluster_data[:,2])
-        avg_phi_value = np.arctan2(avg_y_value, avg_x_value)
-        vertex_color = value_to_hex_color(value=avg_z_value, min_val=-1, max_val=1)
+        avg_phi_value = 2* np.arctan2(avg_y_value, avg_x_value) /np.pi
+        vertex_color = value_to_hex_color(value=avg_phi_value, min_val=-3, max_val=2)
+        if -.1 < avg_phi_value< .2:
+            vertex_opacity = 0.5
+            vertex_color = 0x990000
+            deleted_vertex_set.add(i)
+        else:
+            vertex_opacity = 1
         viz.set_vertex_material(
             vertex_id=i,
             material_props={
                 "color": vertex_color,
-                "size": 15
+                "size": int(5 + len(cluster_data)/50),
+                "opacity": vertex_opacity
             }
         )
 
-
+    edges = simplex_tree.get_simplices(dim=1)
+    # make edges invisible if they link to visible edges to invisible edges
+    for edge in edges:
+        if len(list(set(edge) & set(deleted_vertex_set))) == 1:
+            viz.set_edge_material(source_id=edge[0], target_id=edge[-1],
+                                  material_props={
+                                      "opacity": 0.0001,
+                                  })
+        # make edges dotted if they link between invisible edges
+        if len(list(set(edge) & set(deleted_vertex_set))) == 2:
+            viz.set_edge_material(source_id=edge[0], target_id=edge[-1],
+                                  material_props={
+                                      "color": 0x550202,
+                                      "opacity": 1,
+                                      "dashed": True
+                                  })
+    
+    # make faces incident with invisible vertices invisible
+    faces = simplex_tree.get_simplices(dim=2)
+    for face in faces:
+        if list(set(face) & set(deleted_vertex_set)):
+            viz.set_face_material(vertex_ids=face, material_props={
+                "opacity":0.0001,
+            })
     # Run the visualization
     print("\nLaunching visualization with material customizations...")
     viz.show(port=5000)
