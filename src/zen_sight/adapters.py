1d0b57bc5bbe0bf3a089925ea13ec8d7ac0ec957
Jacob Miller <Jamiller219@gmail.com>
2025-05-28 17:39:53 -0500

    add zen_mapper adapter

--- src/zen_sight/adapters.py
+++ src/zen_sight/adapters.py
@@ -1,5 +1,6 @@
 from zen_sight import Sight
 import networkx as nx
+from zen_mapper import MapperResult
 
 def vis_nx(
         G: nx.Graph, 
@@ -44,3 +45,49 @@ def vis_nx(
     )
     sight.show(port=port)
 
+
+def vis_zen_mapper(result: MapperResult, port: int = 5050):
+    nodes = []
+    links = []
+    faces = []
+
+    for i in result.nerve.get_simplices(dim=0):
+        nodes.append(
+            {
+                "id": f"{i[0]}",
+                "name": f"Node {i[0]}",
+            }
+        )
+    for edge in result.nerve.get_simplices(dim=1):
+        links.append(
+            {
+                "source": f"{edge[0]}",
+                "target": f"{edge[1]}",
+            }
+        )
+
+    faces = result.nerve.get_simplices(dim=2)
+
+    sight = Sight()
+
+    sight.set_nodes(nodes)
+    sight.set_links(links)
+    sight.set_faces(faces)
+
+    sight.set_config(
+        {
+            "nodeAutoColorBy": "group",
+            "nodeRelSize": 4,
+            "nodeLabel": "name",
+            "linkColor": "#000000",
+            "linkWidth": 1,
+            "backgroundColor": "#f2f2f2",
+            # Simplex appearance: alpha is for 2D version and will be ignored
+            # in favor of faceOpacity for 3D
+            "faceFillColor": "rgba(52, 152, 219, 0.3)",
+            "faceStrokeColor": "rgba(52, 152, 219, 0.5)",
+            "faceStrokeWidth": 1,
+            "faceOpacity": 0.3,
+        }
+    )
+    sight.show(port=port)
